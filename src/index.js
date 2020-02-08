cede4c2a7161a64b35f6b4e6d3a93c5444fc1237
Steve-xmh <Steve_xmh@163.com>
2020-02-09 00:03:39 +0800

    Added code compare

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 'use strict'
-const { promises: fs } = require('fs')
-const codeComparer = require('./codeComparer')
-const readline = require('readline')
-const summary = require('./summary')
+// const summary = require('./summary')
+const prettydiff = require('prettydiff')
 const md5 = require('md5')
 const jszip = require('jszip')
 
@@ -13,8 +11,189 @@ const ignoreMD5List = [
     'cd21514d0531fdffb22204e0ec5ed84a',
     'b7853f557e4426412e64bb3da6531a99',
     'ee1c4946b2fbefa4479c1cd60653fb46',
+    'd6118ccd46404fb73d4e9454d610b9ac',
+    '8b8a0a49e7f5ece89ada8c069bf5a7ff',
+    '547019f968041480c14ddf76113bae3d',
+    '7085b3e5732e3689a4ba6a8ad98be814',
+    '9e4bdaa40445a5cf843ffb031838b295',
+    'b578feebd8a0bdba72e38dc61887cce1',
+    'd8e8f89e256b5c131593c0919581a34f',
     ''
 ]
+
+class Block {
+    // 传入工程模块原型创建模块对象
+    constructor (protoBlock) {
+        this.opcode = protoBlock.opcode
+        this.next = protoBlock.next
+        this.parent = protoBlock.parent
+        this.inputs = protoBlock.inputs
+        this.fields = protoBlock.fields
+        this.shadow = protoBlock.shadow
+        this.topLevel = protoBlock.topLevel
+        this.x = protoBlock.x
+        this.y = protoBlock.y
+    }
+
+    get type () { return 'steve-block' };
+    // 转换成字符串
+    toFormatedString (indent = 0) {
+        let ret = `${this.topLevel ? 'function ' : ''}${this.opcode}()${this.topLevel ? '' : ';'}`
+        if (this.opcode === 'control_if' ||
+            this.opcode === 'control_if_else') {
+            ret = 'if(true){'
+        } else if (this.opcode === 'control_forever' ||
+            this.opcode === 'control_repeat' ||
+            this.opcode === 'control_repeat_until' ||
+            this.opcode === 'control_while' ||
+            this.opcode === 'control_for_each') {
+            ret = 'while(true)'
+        }
+        if (this.topLevel) ret += '{'
+        if (this.next) {
+            ret += ` ${this.next.toFormatedString(indent + 1)}`
+        }
+        if (this.inputs.SUBSTACK) {
+            ret += `{${this.inputs.SUBSTACK[1].toFormatedString(indent + 1)}}`
+        }
+        if (this.inputs.SUBSTACK2) {
+            ret += `else{${this.inputs.SUBSTACK2[1].toFormatedString(indent + 1)}}`
+        }
+        if (this.topLevel ||
+            this.opcode === 'control_if' ||
+            this.opcode === 'control_if_else') ret += `${'\t'.repeat(indent)}}`
+        return ret
+    };
+}
+
+// 将模块链接成一个树表
+function fixBlock (block, blocks) {
+    if (typeof block !== 'object') throw new Error('没有传入模块参数！')
+    if (typeof blocks !== 'object') throw new Error('没有传入模块表参数！')
+
+    const b = new Block(block)
+    if (b.next) {
+        b.next = fixBlock(blocks[b.next], blocks)
+        b.next.parent = b
+    }
+    if (b.inputs.SUBSTACK && b.inputs.SUBSTACK[1]) {
+        b.inputs.SUBSTACK[1] = fixBlock(blocks[b.inputs.SUBSTACK[1]], blocks)
+        b.inputs.SUBSTACK[1].parent = b
+    } else {
+        b.inputs.SUBSTACK = undefined
+    }
+
+    if (b.inputs.SUBSTACK2 && b.inputs.SUBSTACK2[1]) {
+        b.inputs.SUBSTACK2[1] = fixBlock(blocks[b.inputs.SUBSTACK2[1]], blocks)
+        b.inputs.SUBSTACK2[1].parent = b
+    } else {
+        b.inputs.SUBSTACK2 = undefined
+    }
+
+    return b
+}
+
+function compareCode (project0, project1) {
+    function makeCodeTree (project) {
+        const array = []
+        for (const target of project.targets) {
+            for (const id in target.blocks) {
+                const block = target.blocks[id]
+                if (block instanceof Object && !(block instanceof Array)) { // Array is an independent variable
+                    if (block.opcode &&
+                        block.opcode.startsWith('event_') &&
+                        block.opcode !== 'event_broadcast') { // Event hat block
+                        array.push({
+                            id,
+                            code: fixBlock(block, target.blocks).toFormatedString()
+                        })
+                    } else if (block.opcode) {
+                        switch (block.opcode) { // Some special hat blocks.
+                        case 'procedures_definition':
+                        case 'control_start_as_clone':
+                            array.push({
+                                id,
+                                code: fixBlock(block, target.blocks).toFormatedString()
+                            })
+                            break
+                        default:
+                        }
+                    }
+                }
+            }
+        }
+        return array
+    }
+    function compareTree (codeTrees0, codeTrees1) {
+        const result = { same: {}, length: 0 }
+        const sameObj = result.same
+        for (const tree0 of codeTrees0) {
+            prettydiff.options.source = tree0.code
+            let samest = null
+            for (const tree1 of codeTrees1) {
+                let equalsLines = 0
+                let changedLine = 0
+                if (tree0.code === tree1.code) {
+                    sameObj[tree0.id] = {
+                        simularTo: tree1.id,
+                        code0: tree0.code,
+                        code1: tree1.code,
+                        // isEqual: true,
+                        simularty: 1 // They are the same
+                    }
+                    result.length++
+                    break
+                } else { // Use prettydiff
+                    prettydiff.options.diff = tree1.code
+                    const prettyResult = JSON.parse(prettydiff())
+                    prettyResult.diff.forEach(v => {
+                        switch (v[0]) {
+                        case '=':
+                            equalsLines++
+                        // eslint-disable-next-line no-fallthrough
+                        case '+':
+                        case 'r':
+                            changedLine++
+                            break
+                        case '-':
+                            changedLine--
+                            break
+                        }
+                    })
+                    if ((!samest || equalsLines / changedLine > samest.simularty) && equalsLines < changedLine) {
+                        samest = {
+                            simularTo: tree1.id,
+                            simularty: equalsLines / changedLine,
+                            code0: tree0.code,
+                            code1: tree1.code
+                        }
+                    }
+                    // console.log(prettyResult)
+                }
+            }
+            if (!sameObj[tree0.id] && samest && samest.simularty > 0.8) {
+                sameObj[tree0.id] = {
+                    simularTo: samest.simularTo,
+                    simularty: samest.simularty,
+                    code0: samest.code0,
+                    code1: samest.code1
+                }
+                result.length++
+            }
+        }
+        return result
+    }
+    const result = {}
+    const codeTrees0 = makeCodeTree(project0)
+    const codeTrees1 = makeCodeTree(project1)
+    prettydiff.options.diff_format = 'json'
+    result.code0length = codeTrees0.length
+    result.code1length = codeTrees1.length
+    result.code0 = compareTree(codeTrees0, codeTrees1)
+    result.code1 = compareTree(codeTrees1, codeTrees0)
+    // console.log(codeTrees0[0])
+    return result
+}
 
 /**
  * Compare the same between two objects
@@ -101,7 +280,7 @@ async function compare (project0, project1) {
         (async () => JSON.parse(await zip0.file('project.json').async('string')))(),
         (async () => JSON.parse(await zip1.file('project.json').async('string')))()
     ])
-    
+    result.code = compareCode(projectJson0, projectJson1)
     return result
 }
 
--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 const comparer = require('../src')
 const fs = require('fs')
 const { join } = require('path')
-
+/*
 comparer(fs.readFileSync(join(__dirname, './TestProject0.sb3')), fs.readFileSync(join(__dirname, './TestProject1.sb3')))
     .then((v) => console.log(v))
+*/
+comparer(fs.readFileSync(join(__dirname, './TestProject0.sb3')), fs.readFileSync(join(__dirname, './TestProject1.sb3')))
+    .then(async (v) => {
+        console.log(v)
+        console.log(`Generated json length: ${JSON.stringify(v).length}`)
+    })
